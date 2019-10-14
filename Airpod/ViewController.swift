0206f0036e1699344e6dbdc063d8c28f1a0c107b
ReKucob <burns950330@163.com>
2019-10-15 10:10:03 +1100

    Initial Commit

---
+++ Airpod/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Airpod
+//
+//  Created by Burns on 15/10/19.
+//  Copyright © 2019 Group 6. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
